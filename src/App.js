23de1e305e29c35553d2c65e98b5f768f380a596
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-22 16:54:01 +0530

    created a single-page-app using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { Component } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/header";
+import BlogsList from "./components/BlogsList/blogslist";
+import About from "./components/About/about";
+import Contact from "./components/Contact/contact";
+import NotFound from "./components/NotFound/notfound";
 
 class App extends Component {
   render() {
-    return <p>Hello User<p/>;
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" Component={BlogsList} />
+          <Route exact path="/about" Component={About} />
+          <Route exact path="/contact" Component={Contact} />
+          <Route exact path="/*" Component={NotFound} />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
