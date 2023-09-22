ba064ce9dab67bc2f7341b21400ca362d84ae60a
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-22 17:34:51 +0530

    added react-loader-spinner for the blogsList

--- src/components/BlogsList/blogslist.js
+++ src/components/BlogsList/blogslist.js
@@ -1,35 +1,53 @@
 import { Component } from "react";
+import { TailSpin } from "react-loader-spinner";
 
 import BlogItem from "../BlogItem/blogitem";
 
 import "./blogslist.css";
 
-const blogsData = [
-  {
-    id: 1,
-    title: "Blog 1",
-    imageUrl: "https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png",
-    avatarUrl: "https://assets.ccbp.in/frontend/react-js/avatar-img.png",
-    author: "Author Name",
-    topic: "React.js",
-  },
-  {
-    id: 2,
-    title: "Blog 2",
-    imageUrl: "https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png",
-    avatarUrl: "https://assets.ccbp.in/frontend/react-js/avatar-img.png",
-    author: "Author Name",
-    topic: "React.js",
-  },
-];
-
 class BlogsList extends Component {
+  state = {
+    blogsData: [],
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.getBlogsData();
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch("https://apis.ccbp.in/blogs");
+    const data = await response.json();
+    // console.log(data);
+    const filteredData = data.map((eachBlog) => ({
+      id: eachBlog.id,
+      author: eachBlog.author,
+      imageUrl: eachBlog.image_url,
+      avatarUrl: eachBlog.avatar_url,
+      topic: eachBlog.topic,
+      title: eachBlog.title,
+    }));
+    this.setState({ blogsData: filteredData, isLoading: false });
+  };
+
   render() {
+    const { blogsData, isLoading } = this.state;
     return (
       <div className="blog-list-container">
-        {blogsData.map((item) => (
-          <BlogItem blogData={item} key={item.id} />
-        ))}
+        {isLoading ? (
+          <TailSpin
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        ) : (
+          blogsData.map((item) => <BlogItem blogData={item} key={item.id} />)
+        )}
       </div>
     );
   }
